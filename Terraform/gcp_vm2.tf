9145046d063ae7af1602d917f79a0560c77a40d6
Michael Grote <michael.grote2@web.de>
2024-04-14 10:13:54 +0200

    remove deleten protection of database

--- Terraform/gcp_vm2.tf
+++ Terraform/gcp_vm2.tf
@@ -84,7 +84,7 @@ resource "google_sql_database_instance" "main" {
   name             = "main-instance"
   database_version = "POSTGRES_15"
   region           = "us-central1"
-
+  deletion_protection=false
   settings {
     # Second-generation instance tiers are based on the machine
     # type. See argument reference below.
